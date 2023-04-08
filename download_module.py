668e23045d048577fcf27676aa0361d0a3417295
Amirali Danai <danaiamirali@gmail.com>
2023-04-08 15:26:53 -0400

    Download Module Complete

--- download_module.py
+++ download_module.py
@@ -1,6 +1,7 @@
 import pathlib
-import urllib
+import urllib.request
 import os
+import tarfile
 
 class DataDownloader:
     """
@@ -24,10 +25,15 @@ class DataDownloader:
                 self.file_names = file_name
         else:
             self.file_names = []
-            for file in list(pathlib.path(url).iterdir()):
+            for file in list(pathlib.Path(url).iterdir()):
                 self.file_names.append(os.path.basename(file))
+
+    def __str__(self):
+        print("URL: ", self.url)
+        print("File names: ", self.file_names)
     
-    # Helper function to make directory
+    # Helper function to make local directory to store the data
+    # Returns created path
     def mkdir(self, path):
         # Make a directory
         path = os.path.join(self.target_dir, path)
@@ -36,8 +42,28 @@ class DataDownloader:
         return path
 
     # Downloads data
-    def download(self):
+    def download(self, keep_zip=False):
         for file in self.file_names:
-            path = os.path.join(self.mkdir(file), file)
-            print(f"Downloading {file}... to {path}")
+            # Local Path to store the data
+            file_dir = self.mkdir(file)
+            file_path = os.path.join(file_dir, file)
+
+            print(f"Downloading {file}... to {file_dir}")
+
+            # Download the data
+            urllib.request.urlretrieve(self.url + file, file_path)
+            extension = file.split(".")[-1]
+            print("Filetype: ", extension)
+
+            tars = ["tar", "tgz", "bz2", "gz", "xz"]
+            if extension == "zip":
+                # TODO: Implement zip file extraction
+                pass
+            elif extension in tars:
+                tarfile.open(file_path, 'r').extractall(path=file_dir)
+                if not keep_zip:
+                    os.remove(file_path)
+            else:
+                raise Exception(f"Filetype {extension} not supported")
+
             print("Done!")
