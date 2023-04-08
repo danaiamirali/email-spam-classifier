3d24f9dd7d3b12f26daae4257cd7a1544752dcb0
Amirali Danai <danaiamirali@gmail.com>
2023-04-08 14:29:31 -0400

    Download_Module Buggy Implementation
    Ctor never invoked according to lack of print statement

--- download_module.py
+++ download_module.py
@@ -1,25 +1,43 @@
 import pathlib
+import urllib
 import os
 
 class DataDownloader:
     """
     Class that takes in URL and downloads the data at said URL.
+    url: URL of the data
+    file_name: Vector of specific files to be downloaded from URL
+    target_dir: Prespecificed directory to download data to
     """
-    def __init__(self, url):
-        self.url = url
-        self.file_names = []
-        for file in list(pathlib.path(url).iterdir()):
-            self.file_names.append(os.path.basename(file))
+    target_dir = "datasets/"
 
-    # Overloaded constructor for specific file name
-    def __init__(self, url, file_name):
+    # Overloaded constructor for two cases:
+    # 1. Downloading specified file(s) from URL
+    # 2. Downloading all files from URL
+    def __init__(self, url, file_name=None):
+        print("Ctor called")
         self.url = url
-        self.file_names = [file_name]
+        if file_name is not None:
+            if not isinstance(file_name, list):
+                self.file_names = [file_name]
+            else:
+                self.file_names = file_name
+        else:
+            self.file_names = []
+            for file in list(pathlib.path(url).iterdir()):
+                self.file_names.append(os.path.basename(file))
+    
+    # Helper function to make directory
+    def mkdir(self, path):
+        # Make a directory
+        path = os.path.join(self.target_dir, path)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        return path
 
+    # Downloads data
     def download(self):
-        # Download the data
-        pass
-
-    def save(self):
-        # Save the data
-        pass
+        for file in self.file_names:
+            path = os.path.join(self.mkdir(file), file)
+            print(f"Downloading {file}... to {path}")
+            print("Done!")
