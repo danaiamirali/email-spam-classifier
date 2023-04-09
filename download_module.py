d03cf098372af9ff2334689e8fa0f5a7a8f13d93
Amirali Danai <danaiamirali@gmail.com>
2023-04-09 06:37:18 -0400

    DM Zip & All-File Download Support

--- download_module.py
+++ download_module.py
@@ -20,15 +20,15 @@ class DataDownloader:
     def __init__(self, url, file_name=None):
         print("Ctor called")
         self.url = url
+        # Specified files
         if file_name is not None:
             if not isinstance(file_name, list):
                 self.file_names = [file_name]
             else:
                 self.file_names = file_name
         else:
-            self.file_names = []
-            for file in list(pathlib.Path(url).iterdir()):
-                self.file_names.append(os.path.basename(file))
+            self.file_names = self.file_names_from_url()
+            
 
     def __str__(self):
         print("URL: ", self.url)
@@ -42,6 +42,16 @@ class DataDownloader:
         if not os.path.exists(path):
             os.mkdir(path)
         return path
+    
+    def file_names_from_url(self):
+        # Get the file names from the URL
+        file_names = []
+        with urllib.request.urlopen(self.url) as response:
+            for line in response:
+                decoded_line = line.decode("utf-8")
+                if decoded_line.startswith("<a href="):
+                    file_names.append(decoded_line.split('"')[1])
+        return file_names
 
     # Downloads data
     def download(self, keep_zip=False):
